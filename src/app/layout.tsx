7f428ecf29120eebf53261cc0a879289b485f29e
AhmadKheder <ahmadkhederabdulatef@gmail.com>
2025-03-25 05:12:02 +0100

    Theme switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,11 @@
 
 import { AppSidebar } from "@/components/app-sidebar";
 import LanguageSwitcher from "@/components/LanguageSwitcher";
+import { ModeToggle } from "@/components/ModeToggle";
 import TranslationProvider from "@/components/TranslationProvider";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import ReduxProvider from "@/store/provider";
+import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
 export default function RootLayout({
@@ -13,22 +15,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className=" max-2-2xl mx-auto p-4 lg:px-8 ">
         <ReduxProvider>
-          <TranslationProvider>
-            <SidebarProvider>
-              <AppSidebar />
-              <div className="flex flex-col w-full h-screen">
-                <div className="flex flex-row justify-between w-full h-screen items-center">
-                  <SidebarTrigger />
-                  <LanguageSwitcher />
-                </div>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <TranslationProvider>
+              <SidebarProvider>
+                <AppSidebar />
+                <div className="flex flex-col w-full h-screen">
+                  <div className="flex flex-row justify-between w-full h-screen items-center">
+                    <div className="flex gap-2">
+                      <SidebarTrigger />
+                      <ModeToggle />
+                    </div>
+                    <LanguageSwitcher />
+                  </div>
 
-                {children}
-              </div>
-            </SidebarProvider>
-          </TranslationProvider>
+                  {children}
+                </div>
+              </SidebarProvider>
+            </TranslationProvider>
+          </ThemeProvider>
         </ReduxProvider>
       </body>
     </html>
---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+  const handleThemeChange = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <Button variant="outline" size="sm" onClick={handleThemeChange}>
+      {theme === "light" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+    </Button>
+  );
+}
